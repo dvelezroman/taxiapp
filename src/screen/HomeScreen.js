f6a15765b084e3ed29417715cbed5216b04d2812
Dario Velez Roman <dario.velez@dinmax.com>
2019-08-20 00:13:44 -0300

    install react-native-maps

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,44 +1,57 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Header, Content, Button, Title, Right, Left, Body } from 'native-base';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { Container, Header, Content, Button, Title, Right, Left, Body, View } from 'native-base';
 import { logout } from '../redux/creators/user';
 import ROUTES from '../router/Routes';
 
 const mapStateToProps = state => ({
-  loggedUser: state.userReducer,
+	loggedUser: state.userReducer
 });
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout()),
+	logout: () => dispatch(logout())
 });
 
 class HomeScreen extends React.Component {
-  logout = () => {
-    this.props.logout();
-    this.props.navigation.navigate(ROUTES.LOGIN);
-  };
+	logout = () => {
+		this.props.logout();
+		this.props.navigation.navigate(ROUTES.LOGIN);
+	};
 
-  render() {
-    return (
-      <Container>
-        <Header>
-          <Left />
-          <Body>
-            <Title>Home</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <Button onPress={() => this.logout()}>
-            <Title>Log Out</Title>
-          </Button>
-        </Content>
-      </Container>
-    );
-  }
+	render() {
+		return (
+			<Container>
+				<Header>
+					<Left>
+						<Button transparent onPress={() => this.logout()}>
+							<Title>Back</Title>
+						</Button>
+					</Left>
+					<Body>
+						<Title>Home</Title>
+					</Body>
+					<Right />
+				</Header>
+				<Content>
+					<View style={{ height: 400, width: 400, justifyContent: 'center', alignItems: 'center' }}>
+						<MapView
+							provider={PROVIDER_GOOGLE}
+							region={{
+								latitude: 37.78825,
+								longitude: -122.4324,
+								latitudeDelta: 0.015,
+								longitudeDelta: 0.0121
+							}}
+						/>
+					</View>
+				</Content>
+			</Container>
+		);
+	}
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(HomeScreen);
